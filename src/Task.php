05fb2053346a32359d90453dfb881ed7557fb6d1
Jared Beckler <jared.beckler@gmail.com>
2016-02-22 02:15:50 -0800

    add functionality for storing inputs in a database

--- src/Task.php
+++ src/Task.php
@@ -37,7 +37,7 @@ class Task
 
     static function deleteAll()
     {
-        $_SESSION['list_of_tasks'] = array();
+        $GLOBALS['DB']->exec("DELETE FROM tasks;");
     }
 }
  ?>
--- tests/TaskTest.php
+++ tests/TaskTest.php
@@ -7,13 +7,18 @@
 
     require_once 'src/Task.php';
 
-    $server = 'mysql:host=localhost;dbname=to_do_test';
+    $server = 'mysql:host=localhost:8889;dbname=to_do_test';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
 
     class TaskTest extends PHPUnit_Framework_TestCase
     {
+        protected function tearDown()
+        {
+            Task::deleteAll();
+        }
+
         function test_save()
         {
             $description = 'Wash the dog';
@@ -37,6 +42,21 @@
             $result = Task::getAll();
 
             $this->assertEquals([$test_task, $test_task2], $result);
+        }
+
+        function test_deleteAll()
+        {
+            $description = "Wash the dog";
+            $description2 = "Water the lawn";
+            $test_task = new Task($description);
+            $test_task->save();
+            $test_task2 = new Task($description2);
+            $test_task2->save();
+
+            Task::deleteAll();
+
+            $result = Task::getAll();
+            $this->assertEquals([], $result);
         }
     }
 
